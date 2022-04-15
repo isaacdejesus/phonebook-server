a5db3e546437d5d532df999b93e641acc70fc43e
isaac <idreyes@uh.edu>
2022-04-15 17:39:25 -0500

    added mongo validators

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const { response } = require('express')
 const logger = require('morgan')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Record = require('./models/record.js')
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-let records =[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 logger('tiny')
 app.use(logger('combined'))
 
@@ -36,41 +16,42 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/records', (request, response) => {
-    response.json(records)
+    Record.find({}).then(records => {
+        response.json(records)
+    })
 })
-app.get('/api/records/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const record = records.find(record => record.id === id)
-    if (record)
-        response.json(record)
-    else
-        response.status(404).end()
-})
-app.delete('/api/records/:id', (request, response) => {
-    const id = Number(request.params.id)
-    records = records.filter(record => record.id !== id)
-   response.status(204).end() 
+app.get('/api/records/:id', (request, response, next) => {
+    Record.findById(request.params.id)
+        .then(record => {
+            if(record)
+                response.json(record)
+            else
+                response.status(404).end()
+    })
+        .catch(error => next(error))
+    })
+app.delete('/api/records/:id', (request, response, next) => {
+    Record.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end() 
+        })
+        .catch(error => next(error))
 })
 app.post('/api/records', (request, response) => {
     const body = request.body
-    if(!body.name || !body.number){
+    if(body.name === undefined|| body.number ===undefined){
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    if(records.find(record => record.name === body.name))
-    {
-        return response.status(400).json({
-            error: "name already in phonebook"
-        })
-    }
-    const record = {
-        id: generateId(),
+        const record = new Record( {
         name: body.name,
         number: body.number
-    }
-    records = records.concat(record)
-    response.json(record)
+    })
+    record.save().then(savedRecord => {
+        response.json(savedRecord)
+    })
+    .catch(error => next(error))
 })
 const generateId = () => {
     return  Math.floor(Math.random() * 92837983798)
@@ -82,7 +63,31 @@ app.get('/info', (request, response) => {
 
     )
 })
-const PORT = process.env.PORT || 3001 
+app.put('/api/records/:id', (request, response, next) => {
+    const body = request.body
+    const record = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Record.findByIdAndUpdate(request.params.id, record, {new: true, runValidators: true})
+        .then(updatedRecord => {
+            response.json(updatedRecord)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
---
+++ models/record.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+console.log('connectiong to', url)
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB:', error.message)
+    })
+
+const recordSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{8}/.test(v);
+            },
+        },
+        required: true
+    }
+})
+
+recordSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
